8d94feb52eb93f7c5da9defc637982cc35719df4
Margarita <savmargo98@gmail.com>
2023-08-28 15:29:27 +0800

    seminar1/

---
+++ seminar1/Products.java
@@ -0,0 +1,10 @@
+package seminar1;
+/**
+     * Товар
+     */
+    
+public class Products {
+   String name; // наименование
+   String brand; // производитель
+   double price;
+}
---
+++ seminar1/programm.java
@@ -0,0 +1,9 @@
+package seminar1;
+
+public class programm {
+  public static void main(String[] args) {
+    Products product1 = new Products();
+    product1.name = "prod1";
+    product1.price = 111;
+  }  
+}
